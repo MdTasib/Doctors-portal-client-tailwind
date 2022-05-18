daadc06cd9b160d88e64a5f6c3c0bb9696dd5393
MdTasib <ohidulalam442@gmail.com>
2022-05-18 12:31:22 +0600

    payment intrigate

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,7 +1,14 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../Shared/Loading";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+	"pk_test_51IeAZaFLS713LSSjm5z39rde0Dw558BCbCNNKnHZznQzN9khB8LRy5SdjLKzazWus6sKcNwr70ajIgGmWBloqgCy00pmDjThkv"
+);
 
 const Payment = () => {
 	const { id } = useParams();
@@ -38,7 +45,11 @@ const Payment = () => {
 				</div>
 			</div>
 			<div class='card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100'>
-				<div class='card-body'></div>
+				<div class='card-body'>
+					<Elements stripe={stripePromise}>
+						<CheckoutForm />
+					</Elements>
+				</div>
 			</div>
 		</div>
 	);
