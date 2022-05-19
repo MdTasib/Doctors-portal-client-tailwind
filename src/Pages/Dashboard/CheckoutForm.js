1c626b05a2e72f98e939ba4582ba9417916d70ab
MdTasib <ohidulalam442@gmail.com>
2022-05-19 12:09:18 +0600

    get client Secret from stripe

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,9 +1,32 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ appointment }) => {
 	const stripe = useStripe();
 	const elements = useElements();
+	const [cardError, setCardError] = useState("");
+	const [clientSecret, setClientSecret] = useState("");
+
+	console.log("clinent sectret", clientSecret);
+
+	const { price } = appointment;
+
+	useEffect(() => {
+		fetch("http://localhost:5000/create-payment-intent", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+				authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+			},
+			body: JSON.stringify({ price }),
+		})
+			.then(res => res.json())
+			.then(data => {
+				if (data?.clientSecret) {
+					setClientSecret(data.clientSecret);
+				}
+			});
+	}, [price]);
 
 	const handleSubmit = async event => {
 		event.preventDefault();
@@ -25,37 +48,44 @@ const CheckoutForm = () => {
 		});
 
 		if (error) {
-			console.log("[error]", error);
+			setCardError(error?.message);
 		} else {
+			setCardError("");
 			console.log("[PaymentMethod]", paymentMethod);
+			if (paymentMethod) {
+				console.log("success");
+			}
 		}
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<CardElement
-				options={{
-					style: {
-						base: {
-							fontSize: "16px",
-							color: "#424770",
-							"::placeholder": {
-								color: "#aab7c4",
+		<>
+			<form onSubmit={handleSubmit}>
+				<CardElement
+					options={{
+						style: {
+							base: {
+								fontSize: "16px",
+								color: "#424770",
+								"::placeholder": {
+									color: "#aab7c4",
+								},
+							},
+							invalid: {
+								color: "#9e2146",
 							},
 						},
-						invalid: {
-							color: "#9e2146",
-						},
-					},
-				}}
-			/>
-			<button
-				type='submit'
-				className='btn-secondary btn btn-sm text-white mt-10'
-				disabled={!stripe}>
-				Pay
-			</button>
-		</form>
+					}}
+				/>
+				<button
+					type='submit'
+					className='btn-secondary btn btn-sm text-white mt-10'
+					disabled={!stripe || !clientSecret}>
+					Pay
+				</button>
+			</form>
+			{cardError && <small className='text-red-500'>{cardError}</small>}
+		</>
 	);
 };
 
